101dc97dd9c54c8f7d9a1ffe1b74caf9fafc3283
Chayan Sarkar <59403799+apu-hub@users.noreply.github.com>
2022-09-15 10:50:30 +0530

    are you ready

--- action-test/index.js
+++ action-test/index.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import FormData from "form-data"
 import fs from "node:fs/promises"
 
-const image = await fs.readFile('./hello.text');
+const apk = await fs.readFile('./hello.text');
 
-// Create a form and append image with additional fields
+// Create a form and append apk with additional fields
 const form = new FormData();
 // form.append('productName', 'Node.js Stickers');
 // form.append('productDescription', 'Cool collection of Node.js stickers for your laptop.');
-form.append('productImage', image, 'hello.text');
+form.append('apk', apk, 'hello.text');
 
 // Send form data with axios
 const response = await axios.post('https://susa.deta.dev/from-github/upload', form, {
@@ -17,4 +17,4 @@ const response = await axios.post('https://susa.deta.dev/from-github/upload', fo
   },
 });
 
-console.log(response);   // Only Dev
+console.log(response.data);   // Only Dev
