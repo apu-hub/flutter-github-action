802fd7ff36eba9bb0c5c614ced07d31e95ff0ac4
Chayan Sarkar <59403799+apu-hub@users.noreply.github.com>
2023-01-29 17:32:57 +0530

    add new server

--- action-test/index.js
+++ action-test/index.js
@@ -5,19 +5,23 @@ import fs from "node:fs/promises"
 const osPath = "/home/runner/work/flutter-github-action/flutter-github-action"
 const basePath = process.env.BASE_PATH || "/build/app/outputs/flutter-apk"
 const fileName = process.env.FILE_NAME || "/app-release.apk";
-const filePath =osPath + basePath + fileName;
+const filePath = osPath + basePath + fileName;
 
 const file = await fs.readFile(filePath);
 
 const form = new FormData();
 
+console.log(process.env.UPLOAD_SECRET_KEY);   // Only Dev
+
 form.append('file', file, fileName);
+data.append('name', 'latest');
+data.append('secret_key', process.env.UPLOAD_SECRET_KEY || '');
 
 // Send form data with axios
-const response = await axios.post('http://test-123.epizy.com/upload/tazzatv/index.php', form, {
+const response = await axios.post('http://ec2-3-110-167-11.ap-south-1.compute.amazonaws.com:3000/upload', form, {
   maxContentLength: Infinity,
   maxBodyLength: Infinity,
-  headers: {'Content-Type': 'multipart/form-data;boundary=' + form.getBoundary()}
+  headers: { 'Content-Type': 'multipart/form-data;boundary=' + form.getBoundary() }
 });
 
 console.log(response.data);   // Only Dev
